0354ed1c79f4e7b39ef67dcca3f34017ca7fc5ad
razaqultegar <myrazaqul@gmail.com>
2019-03-20 09:25:48 +0700

    ✨  new login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  public url = 'https://m.me/puskomedia.id';
+
+// tslint:disable-next-line: no-empty
+  constructor() { }
+
+// tslint:disable-next-line: no-empty
+  public ngOnInit() {
+  }
+
+}
